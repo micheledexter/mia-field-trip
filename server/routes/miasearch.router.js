182f606cf6113118fa2aa64b8584eaa50e084c4c
Alex Aspell <alexgaspell@gmail.com>
2018-08-05 18:51:23 -0500

    calling mia api

--- server/routes/miasearch.router.js
+++ server/routes/miasearch.router.js
@@ -2,3 +2,13 @@ const express = require('express');
 const pool = require('../modules/pool');
 const axios = require('axios');
 const router = express.Router();
+
+axios.get('http://search.artsmia.org/')
+.then( res => {
+    let response = res.data;
+    console.log('response', response);
+})
+.catch( error => {
+    console.log('Error getting API', error);
+})
+module.exports = router;
