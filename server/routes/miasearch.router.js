3a3c38e1ef5fee7e747d204ab2ea70227937a1fa
Alex Aspell <alexgaspell@gmail.com>
2018-08-05 19:06:12 -0500

    receiving collection data

--- server/routes/miasearch.router.js
+++ server/routes/miasearch.router.js
@@ -3,10 +3,10 @@ const pool = require('../modules/pool');
 const axios = require('axios');
 const router = express.Router();
 
-axios.get('http://search.artsmia.org/')
+axios.get('http://search.artsmia.org/armor')
 .then( res => {
-    let response = res.data;
-    console.log('response', response);
+    let hits = res.data.hits.hits;
+    console.log('hits', hits);
 })
 .catch( error => {
     console.log('Error getting API', error);
